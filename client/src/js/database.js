c0c25eb5c17b5c9e90af4d4847600b0e599eec5d
evan-hatley <143963975+evan-hatley@users.noreply.github.com>
2024-02-10 20:07:25 -0600

    SRC Database and Install

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -4,7 +4,7 @@ const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+        console.log('This database already exists');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readWrite');
+  const store = tx.objectStore('jate');
+  const request = store.add(content);
+  const result = await request;
+  console.log('Results!', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readOnly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('Results!', result);
+  return result?.value;
+};
 
 initdb();
